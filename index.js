2fc65e1d922bb6204a069d7547dfc5fb3b5faeef
Steve <stephenvincentibanez@gmail.com>
2020-11-15 19:52:26 -0800

    commit tree 0f896e47dead86a747678fff7e7ca748d199ecf0

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solutions in this file
+function writeCards(arr, event_name){
+    for (let i = 0; i < arr.length; i++){
+        console.log(`Thank you, ${arr[i]}, for the wonderful ${event_name} gift!`)
+    }
+}
+
+function countDown(integer){
+    let i = 10;
+    while (i > 0) {
+        console.log(i--);
+    }
+}
